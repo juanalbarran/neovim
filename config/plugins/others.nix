4ec6175b9a3f61d3834805cfcb533a9a4780f4c4
Juan Jesus <juanjesusalbarran@gmail.com>
2025-09-28 13:48:33 +0200

    adding providers in a different way

--- config/plugins/others.nix
+++ config/plugins/others.nix
@@ -2,7 +2,14 @@
 { pkgs, ... }:
 {
   extraPackages = with pkgs; [
-    python3
+    # Python provider (needed by most plugins)
+    (pkgs.python3.withPackages (ps: with ps; [ pynvim ]))
+
+    # Node.js provider (for plugins like coc.nvim, etc.)
+    (pkgs.nodejs_20)
+
+    # Ruby provider (less common, but still supported)
+    (pkgs.ruby.withPackages (ps: with ps; [ neovim ]))
   ];
 }
 
