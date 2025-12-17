38f6006faeb08ad506e665f3c559ea6b8017758d
Juan Albarran <juan.albarran@nixs.com>
2025-12-17 14:14:48 +0100

    Added a fix to the QML lsp

--- src/packages/neovim/base/__config/lua/lsp-qml.lua
+++ src/packages/neovim/base/__config/lua/lsp-qml.lua
@@ -1,6 +1,5 @@
 -- src/packages/neovim/base/__config/lua/lsp-qml.lua
-
-local blink = require("blink-cmp")
+local blink = require("blink.cmp")
 
 local capabilities = {}
 capabilities = vim.tbl_extend("keep", capabilities, blink.get_lsp_capabilities())
@@ -10,7 +9,23 @@ vim.lsp.config("qmlls", {
 	cmd = { "qmlls" },
 	filetypes = { "qml", "qmljs" },
 	root_dir = function(fname)
-		return vim.fs.dirname(vim.fs.find({ "qmldir", ".git" }, { path = fname, upward = true })[1])
+		-- 1. Guard: Ensure fname is actually a string (fixes the "got number" error)
+		if type(fname) ~= "string" then
+			return nil
+		end
+
+		-- 2. Guard: Skip special buffers like oil:// or fugitive://
+		if vim.startswith(fname, "oil://") or vim.startswith(fname, "fugitive://") then
+			return nil
+		end
+
+		-- 3. Safely find the root
+		local found = vim.fs.find({ "qmldir", ".git" }, { path = fname, upward = true })[1]
+		if found then
+			return vim.fs.dirname(found)
+		end
+
+		return nil
 	end,
 	single_file_support = true,
 })
