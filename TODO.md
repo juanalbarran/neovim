de1146c4d9086e20ae1db765eb0b31e859f4ef9a
Juan Albarran <juan.albarran@nixs.com>
2025-12-27 11:38:56 +0100

    Added background to vague colorscheme, also added TODO list

---
+++ TODO.md
@@ -0,0 +1,15 @@
+# Things to do
+
+These are things that are pending in my neovim config
+
+## UI
+
+[ ] Update background to match ghostty
+
+## QML
+
+[ ] Add formatting to qml language
+
+## JAVA
+
+[ ] Continue the java config
--- src/packages/neovim/base/__config/lua/colorscheme.lua
+++ src/packages/neovim/base/__config/lua/colorscheme.lua
@@ -1,4 +1,9 @@
 -- src/packages/neovim/base/__config/lua/colorscheme.lua
 -- vim.cmd.colorscheme('zenbones')
 -- vim.cmd.colorscheme("alabaster")
+require("vague").setup({
+	colors = {
+		bg = "#131314",
+	},
+})
 vim.cmd.colorscheme("vague")
