be36c864b1ec986ab04e0caf54f1842219117314
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-03-05 15:03:31 +0100

    Salesforce
    Adding Salesforce plugin

---
+++ src/packages/neovim/salesforce/__config/lua/sf-nvim.lua
@@ -0,0 +1,9 @@
+-- src/packages/neovim/salesforce/__config/lua/sf-nvim.lua
+require('sf').setup({
+  -- Default configurations usually work out of the box,
+  -- but you can bind your keys here
+})
+
+-- Example Keymaps for sf.nvim
+-- vim.keymap.set("n", "<leader>sf", "<cmd>SF<CR>", { desc = "Open Salesforce Menu" })
+-- vim.keymap.set("n", "<leader>sp", "<cmd>SF push<CR>", { desc = "Salesforce Push" })
