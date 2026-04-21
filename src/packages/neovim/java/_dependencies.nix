3ce25f7c4bdbb4ade720cf30cc05074b5e8ee308
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-04-21 17:57:12 +0200

    Java
    Lombok again

--- src/packages/neovim/java/_dependencies.nix
+++ src/packages/neovim/java/_dependencies.nix
@@ -1,20 +1,14 @@
 # src/packages/neovim/java/_dependencies.nix
 {pkgs}:
 with pkgs; let
-  # Create a custom wrapper for jdtls that automatically injects the lombok javaagent
   jdtls-with-lombok = writeShellScriptBin "jdtls" ''
-    # JDTLS provided by nixpkgs respects the JDTLS_JVM_ARGS environment variable.
-    # We inject the absolute path to the lombok jar into it.
-    export JDTLS_JVM_ARGS="-javaagent:${lombok}/share/java/lombok.jar ''${JDTLS_JVM_ARGS:-}"
-
-    # Execute the real jdtls with any arguments passed by Neovim
-    exec ${jdt-language-server}/bin/jdtls "$@"
+    exec ${jdt-language-server}/bin/jdtls \
+      --jvm-arg=-javaagent:${lombok}/share/java/lombok.jar \
+      "$@"
   '';
 in [
-  # Drop our custom wrapped jdtls into the environment instead of the bare one
   jdtls-with-lombok
 
-  # The rest of your dependencies
   google-java-format
   temurin-bin
 
